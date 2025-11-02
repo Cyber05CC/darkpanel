9edb3e0a03b19f050525ebfdba2e37beda697686
darken_ae <otashblogs@gmail.com>
2025-11-02 13:29:27 +0500

    ewrdsf

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // ----------------------- UPDATE CONFIG -----------------------
     const REMOTE_BASE = 'https://darkpanel-coral.vercel.app';
     const UPDATE_URL = REMOTE_BASE + '/update.json';
-    const BUNDLE_VERSION = '1.3';
+    const BUNDLE_VERSION = '1.4';
     const LS_INSTALLED = 'darkpanel_installed_version';
     const SUPPORTED_TEXT_FILES = ['index.html', 'css/style.css', 'js/main.js', 'CSXS/manifest.xml'];
     // -------------------------------------------------------------
